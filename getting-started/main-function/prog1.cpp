c32b3933843fcff43678d9be66cc832d788a45d4
Stefan <stefansandorovssb@gmail.com>
2024-01-11 23:33:43 +0100

    .

--- getting-started/main-function/prog1.cpp
+++ getting-started/main-function/prog1.cpp
@@ -4,6 +4,6 @@ int main()
     // A nonzero return has a meaning that is defined by the system. Ordinarily a nonzero return indicates what 
     // kind of error occurred.
     //* to output the result of the execution of the file, we use echo $? in UNIX based OS
-    return 0; // success message 0
+    // return 0; // success message 0
     return -1; // error meesage 255
 }
