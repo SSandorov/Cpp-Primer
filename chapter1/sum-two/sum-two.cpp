aebb04d2124f5d57ed8aaf87143518558c103ca4
Stefan <stefansandorovssb@gmail.com>
2024-01-15 20:19:55 +0100

    fixing issues

--- chapter1/sum-two/sum-two.cpp
+++ chapter1/sum-two/sum-two.cpp
@@ -3,13 +3,13 @@
 int main()
 {
                                         //* std library endline, its called a manipulator
-                                        // it not only ends the line, but it also flushes the buffer associated
-                                        //  Flushing the buffer ensures that all the output the program has generated 
-                                        // so far is actually written to the output stream, rather than sitting 
-                                        // in memory waiting to be written.
+                                        //* it not only ends the line, but it also flushes the buffer associated
+                                        //* Flushing the buffer ensures that all the output the program has generated 
+                                        //* so far is actually written to the output stream, rather than sitting 
+                                        //* in memory waiting to be written.
 
                                         //* Programmers often add print statements during debugging. Such
-                                        //* tatements should always flush the stream. Otherwise, if the program 
+                                        //* statements should always flush the stream. Otherwise, if the program 
                                         //* crashes, output may be left in the buffer, leading to incorrect 
                                         //* inferences about where the program crashed.
     std::cout << "Enter two numbers:" << std::endl;
