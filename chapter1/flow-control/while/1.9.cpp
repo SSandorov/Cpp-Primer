c4c7cea15ec66d8b49e5d0dc2d2676b26089740e
Stefan <stefansandorovssb@gmail.com>
2024-01-16 21:42:45 +0100

    excercise 1.9

---
+++ chapter1/flow-control/while/1.9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int sum = 0, val = 50;
+
+    while(val <= 100) {
+        sum += val;
+
+        ++val;
+    }
+
+    std::cout << "The sum from 50 to 100 is " << sum << std::endl;
+
+    return 0;
+}
