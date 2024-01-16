a0b79be8cbe859dbdefa1d47b4a9b63c4c329e75
Stefan <stefansandorovssb@gmail.com>
2024-01-16 22:26:34 +0100

    exercise 1.11

---
+++ chapter1/flow-control/while/1.11.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int val1 = 0, val2 = 0;
+
+    std::cout << "Please, enter two numbers:" << std::endl;
+
+    std::cin >> val1 >> val2;
+
+    while(val1 <= val2) {
+        std::cout << val1++ << std::endl;
+    }
+
+    return 0;
+
+}
