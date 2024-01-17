d40a674460e4d71b838f7ba32070c05fcf733003
Stefan <stefansandorovssb@gmail.com>
2024-01-17 20:49:37 +0100

    exercise 1.12

--- chapter1/flow-control/for/1.12.cpp
+++ chapter1/flow-control/for/1.12.cpp
@@ -2,5 +2,12 @@
 
 int main()
 {
+    int sum = 0;
+    for (int i = -100; i <= 100; ++i) {
+        sum += i;
+    }
+    std::cout << sum << std::endl;
+    //* Sum the values from -100 to 100. It returns 0
+    
     return 0;
 }
