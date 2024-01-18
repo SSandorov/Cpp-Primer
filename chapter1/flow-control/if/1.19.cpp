ffca840e389774e286f8b7de73db52071ace2d64
Stefan <stefansandorovssb@gmail.com>
2024-01-18 22:30:21 +0100

    exercise 1.19

---
+++ chapter1/flow-control/if/1.19.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    int val1 = 0, val2 = 0;
+
+    std::cout << "Please, enter two numbers:" << std::endl;
+
+    std::cin >> val1 >> val2;
+
+    if (val1 <= val2) {
+        while(val1 <= val2) {
+            std::cout << val1++ << std::endl;
+        }
+    } else {
+        while(val1 >= val2) {
+            std::cout << val1-- << std::endl;
+        }
+    }
+
+    return 0;
+
+}
